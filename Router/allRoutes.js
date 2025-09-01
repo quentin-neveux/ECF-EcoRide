37a77203ce7cc65cb4fb84ae56852c9ebf344c69
quentin-neveux <quentin.neveux1991@gmail.com>
2025-09-01 15:43:28 +0200

    Pages==>done

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,10 +5,9 @@ export const websiteName = "ÉcoRide - Le premier site de covoiturage écologiqu
 export const allRoutes = [
   new Route("/", "Bienvenue", "pages/home.html"),
   new Route("/covoiturer", "Covoiturer", "pages/covoiturer.html"),
-  new Route("/trajets", "Trajets", "pages/les-trajets.html"),
+  new Route("/les-trajets", "Trajets", "pages/les-trajets.html"),
   new Route("/contact", "Contact", "pages/contact.html"),
   new Route("/connexion", "Connexion", "pages/connexion.html"),
   new Route("/inscription", "Inscription", "pages/inscription.html"),
-  new Route("/membres", "Membres", "pages/membres.html")
-
+  // Removed duplicate route for "/inscription" with "Membres"
 ];
